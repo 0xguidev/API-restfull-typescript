cc0d44abae424bf1e58ed55232004bfc08a74022
0xguidev <guilhermeviniciustrabalho@gmail.com>
2023-01-03 15:28:57 -0300

    Feature: class error handle

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,5 +1,6 @@
 import cors from 'cors';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import AppError from '../err/AppError';
 import routes from './routes';
 
 const app = express();
@@ -9,6 +10,20 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response
+        .status(error.statusCode)
+        .json({ status: 'error', message: error.message });
+    }
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
