efc51295318d26aa6b73671043f4872c3b8787b4
0xguidev <guilhermeviniciustrabalho@gmail.com>
2023-01-06 14:11:12 -0300

    Feature: update product service.

---
+++ src/modules/products/services/UpdateProductService.ts
@@ -0,0 +1,42 @@
+import AppError from '@shared/err/AppError';
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Product';
+import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+
+interface IRequest {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+class CreateProductService {
+  public async execute({
+    id,
+    name,
+    price,
+    quantity,
+  }: IRequest): Promise<Product> {
+    const productsRepository = getCustomRepository(ProductRepository);
+    const product = await productsRepository.findOne(id);
+
+    if (!product) {
+      throw new AppError('Product not found');
+    }
+
+    const productExists = await productsRepository.findByName(name);
+
+    if (productExists && name != product.name) {
+      throw new AppError('There is already one product with this name');
+    }
+
+    product.name = name;
+    product.price = price;
+    product.quantity = quantity;
+
+    await productsRepository.save(product);
+
+    return product;
+  }
+}
+export default CreateProductService;
