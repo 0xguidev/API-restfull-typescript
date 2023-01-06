ecf426a7d03fee77d55cdd77077b83187aecd8ba
0xguidev <guilhermeviniciustrabalho@gmail.com>
2023-01-06 13:54:37 -0300

    feature: list all products

---
+++ src/modules/products/services/ListProductService.ts
@@ -0,0 +1,14 @@
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Product';
+import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+
+class ListProductService {
+  public async execute(): Promise<Product[]> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const product = productsRepository.find();
+
+    return product;
+  }
+}
+export default ListProductService;
