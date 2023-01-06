6aaa1da05e0ecc1ba65a769efe6c6f290a76d0a0
0xguidev <guilhermeviniciustrabalho@gmail.com>
2023-01-06 13:58:14 -0300

    Feature: show a product service

---
+++ src/modules/products/services/ShowProductService.ts
@@ -0,0 +1,22 @@
+import AppError from '@shared/err/AppError';
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Product';
+import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+
+interface IRequest {
+  id: string;
+}
+
+class CreateProductService {
+  public async execute({ id }: IRequest): Promise<Product> {
+    const productsRepository = getCustomRepository(ProductRepository);
+    const product = await productsRepository.findOne(id);
+
+    if (!product) {
+      throw new AppError('Product not found');
+    }
+
+    return product;
+  }
+}
+export default CreateProductService;
