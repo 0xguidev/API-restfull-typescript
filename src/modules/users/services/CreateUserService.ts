4183a0b79ad4d0607cbe4b348d90a2a93e0508d4
0xguidev <guilhermeviniciustrabalho@gmail.com>
2023-01-14 13:08:39 -0300

    Feature: create user service

---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,38 @@
+import AppError from '@shared/err/AppError';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+  avatar: string;
+}
+
+export default class CreateUserService {
+  public async execute({
+    name,
+    email,
+    avatar,
+    password,
+  }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const emailExists = await usersRepository.findByEmail(email);
+
+    if (emailExists) {
+      throw new AppError('There is already one user with this email');
+    }
+
+    const user = usersRepository.create({
+      name,
+      avatar,
+      password,
+      email,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
