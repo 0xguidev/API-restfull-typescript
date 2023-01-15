43d1e2faeb6f80d7d7624b533da51a3e153e3cd3
0xguidev <guilhermeviniciustrabalho@gmail.com>
2023-01-15 14:47:02 -0300

    Feature: user routes 2

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,11 @@
 import productsRouter from '@modules/products/routes/products.routes';
+import userRouter from '@modules/users/routes/user.routes';
 import { Router } from 'express';
 
 const routes = Router();
 
 routes.use('/products', productsRouter);
+
+routes.use('/user', userRouter);
 
 export default routes;
