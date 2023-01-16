f7b33a2a73d91cbe85d123c339881a518a2b29d5
0xguidev <guilhermeviniciustrabalho@gmail.com>
2023-01-16 14:50:31 -0300

    Feature: create user authentication service

---
+++ src/modules/users/services/CreateSessionsService.ts
@@ -0,0 +1,31 @@
+import AppError from '@shared/err/AppError';
+import { compare } from 'bcryptjs';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+
+interface IRequest {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+}
+
+export default class CreateSessionsService {
+  public async execute({ id, name, email, password }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const user = await usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('user not found', 401);
+    }
+
+    const passwordConfirmed = await compare(password, user.password);
+
+    if (!passwordConfirmed) {
+      throw new AppError('user not found', 401);
+    }
+
+    return user;
+  }
+}
