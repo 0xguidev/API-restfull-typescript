2fb35e8ffbfdbf204cb8991a54c6ef198b8877e4
0xguidev <guilhermeviniciustrabalho@gmail.com>
2023-01-16 15:02:44 -0300

    Feature: create authentication controller

---
+++ src/modules/users/controllers/CreateSessionController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import CreateSessionsService from '../services/CreateSessionsService';
+
+export default class UsersController {
+  public async create(req: Request, res: Response): Promise<Response> {
+    const { email, password } = req.body;
+
+    const createSession = new CreateSessionsService();
+
+    const user = await createSession.execute({
+      email,
+      password,
+    });
+
+    return res.json(user);
+  }
+}
