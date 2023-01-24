9bd63083caee0727ee6d0886ce540627d3d60b0b
0xguidev <guilhermeviniciustrabalho@gmail.com>
2023-01-24 18:08:57 -0300

    Feature: update user avatar service

---
+++ src/modules/users/services/UpdateuserAvatarService.ts
@@ -0,0 +1,37 @@
+import AppError from '@shared/err/AppError';
+import fs from 'node:fs';
+import path from 'node:path';
+import upload from 'src/configs/upload';
+import { getCustomRepository } from 'typeorm';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+
+interface IRequest {
+  userId: string;
+  avatarFileName: string;
+}
+
+export default class UpdateUserAvatarService {
+  public async execute({ avatarFileName, userId }: IRequest) {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const user = await usersRepository.findById(userId);
+
+    if (!user) throw new AppError('User not found');
+
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(upload.directory, user.avatar);
+
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    user.avatar = avatarFileName;
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
