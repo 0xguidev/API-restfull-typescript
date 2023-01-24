28643ec7cda243ebdc8d06d77b1a1f3f43b4f4e7
0xguidev <guilhermeviniciustrabalho@gmail.com>
2023-01-24 19:32:23 -0300

    Feature: create update avatar endpoint

--- src/modules/users/routes/user.routes.ts
+++ src/modules/users/routes/user.routes.ts
@@ -1,14 +1,29 @@
+import uploadConfig from '@configs/upload';
 import { celebrate, Segments } from 'celebrate';
 import { Router } from 'express';
 import Joi from 'joi';
+import multer from 'multer';
+
 import IsAuthenticated from '../../../shared/http/middleware/IsAutheticated';
+import UserAvatarController from '../controllers/UserAvatarController';
 import UsersController from '../controllers/UsersControllers';
 
 const userRouter = Router();
 
 const userController = new UsersController();
+const userAvatarController = new UserAvatarController();
+
+const upload = multer(uploadConfig);
 
 userRouter.get('/', IsAuthenticated, userController.index);
+
+userRouter.patch(
+  '/avatar',
+  IsAuthenticated,
+  upload.single('avatar'),
+  userAvatarController.update,
+);
+
 userRouter.get(
   '/:id',
   celebrate({
@@ -29,6 +44,7 @@ userRouter.post(
   }),
   userController.create,
 );
+
 userRouter.put(
   '/:id',
   celebrate({
