95fae60f96a5a82d40287d6c3edeb53f46c6694b
0xguidev <guilhermeviniciustrabalho@gmail.com>
2023-01-24 10:15:14 -0300

    Feature: install Multer, create config file to upload files from server

---
+++ src/configs/upload.ts
@@ -0,0 +1,17 @@
+import crypto from 'crypto';
+import multer from 'multer';
+import path from 'node:path';
+
+const UploadFolder = path.resolve(__dirname, '..', '..', 'uploads');
+
+export default {
+  directory: UploadFolder,
+  storage: multer.diskStorage({
+    destination: UploadFolder,
+    filename(request, file, callback) {
+      const fileHash = crypto.randomBytes(10).toString('hex');
+      const filename = `${fileHash}-${file.originalname}`;
+      callback(null, filename);
+    },
+  }),
+};
