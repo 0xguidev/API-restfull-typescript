00462ef56e92407fcf4c8d9777e0c8ebc8a839ac
0xguidev <guilhermeviniciustrabalho@gmail.com>
2023-01-27 10:50:02 -0300

    Feature: Entity UserToken

---
+++ src/modules/users/typeorm/entities/UserToken.ts
@@ -0,0 +1,26 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Generated,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('users')
+export default class UserToken {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  @Generated('uuid')
+  token: string;
+
+  @Column()
+  user_id: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @CreateDateColumn()
+  updated_at: Date;
+}
