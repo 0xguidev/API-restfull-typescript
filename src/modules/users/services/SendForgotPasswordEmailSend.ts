6e043cd4d4a08650c40ded6f733e77783fc1351d
0xguidev <guilhermeviniciustrabalho@gmail.com>
2023-01-31 10:20:04 -0300

    Feature: Initializate send forgot password service

---
+++ src/modules/users/services/SendForgotPasswordEmailSend.ts
@@ -0,0 +1,21 @@
+import AppError from '@shared/err/AppError';
+import { getCustomRepository } from 'typeorm';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
+
+interface IRequest {
+  email: string;
+}
+
+export default class SendForgotPasswordEmailService {
+  public async execute({ email }: IRequest): Promise<void> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokenRepo = getCustomRepository(UserTokensRepository);
+
+    const user = await usersRepository.findByEmail(email);
+
+    if (!user) throw new AppError('User do not exists');
+
+    const token = userTokenRepo.generate(user.id);
+  }
+}
